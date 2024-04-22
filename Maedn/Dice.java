e70790eacb9b94a2648de2bdbb9c037fd5104b05
LeonTM <lanneken09@gmail.com>
2024-04-22 21:52:57 +0200

    Updated the Roll function

--- Maedn/Dice.java
+++ Maedn/Dice.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Dice here.
+ * This is the dice class.
  * 
  * @author (your name) 
  * @version (a version number or a date)
@@ -12,8 +12,23 @@ public class Dice extends Actor
      * Act - do whatever the Dice wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public int currentNumber = 1;
+    public boolean isRolled = false;
     public void act()
     {
         // Add your action code here.
+    }
+    public void Roll()
+    {
+        if (!isRolled) {
+            int spinDice = Greenfoot.getRandomNumber(3) + 1;
+            for (int i = 0; i < spinDice; i++)
+            {
+                int number = Greenfoot.getRandomNumber(6) + 1;
+                setImage(new GreenfootImage("dice" + number + ".png"));
+                Greenfoot.delay(0.5);
+            }
+            isRolled = true;
+        }
     }
 }
