397f979d4831f92d360d10b19abc9b422f7ef7be
Gianluca <88041565+gianlucaNie@users.noreply.github.com>
2024-04-24 21:00:11 +0200

    .

--- Maedn/Dice.java
+++ Maedn/Dice.java
@@ -14,6 +14,7 @@ public class Dice extends Actor
      */
     public static int currentNumber = 1;
     public static boolean isRolled = false;
+
     public void act()
     {
         if (Greenfoot.mouseClicked(this))
@@ -24,7 +25,7 @@ public class Dice extends Actor
     public void Roll()
     {
         if (!isRolled) {
-            int spinDice = Greenfoot.getRandomNumber(6) + 1;
+            int spinDice = Greenfoot.getRandomNumber(4) + 2;
             for (int i = 0; i < spinDice; i++)
             {
                 int number = Greenfoot.getRandomNumber(6) + 1;
