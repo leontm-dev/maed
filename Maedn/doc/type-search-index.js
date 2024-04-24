16c4eda57ee2123adc4a5f457f58cd60cd5e0ace
LeonTM <lanneken09@gmail.com>
2024-04-24 16:33:36 +0200

    Project docs

---
+++ Maedn/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Cell"},{"p":"<Unnamed>","l":"Dice"},{"p":"<Unnamed>","l":"GameBoard"},{"p":"<Unnamed>","l":"Player"}];updateSearchResults();
